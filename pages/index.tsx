f5ecbcdf9e91ccf2eaf276d52a539e28f61b9506
Marvin Kome <marvin.kome@movinga.com>
2022-09-28 15:36:09 +0200

    add scrolling effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import { createRef, useEffect, useMemo, useRef, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/future/image";
-import { motion, useScroll } from "framer-motion";
-
-const AnimatedText = () => {};
+import { motion, useMotionTemplate, useScroll, useTransform } from "framer-motion";
 
 const ALL_IMAGES = [
   {
@@ -19,143 +16,40 @@ const ALL_IMAGES = [
     src: "/images/3.jpg",
     alt: "Girl with denim shirt",
   },
-  {
-    src: "/images/4.jpg",
-    alt: "Girl with glasses",
-  },
 ];
 
-const ImageList = () => {
-  const [images, setImages] = useState(ALL_IMAGES);
-  const imageScrollRef = useRef<HTMLDivElement>(null);
-  const imageRefs = useRef<any[]>([]);
+const Image = ({ id }: any) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const radius = useTransform(scrollYProgress, [0, 0.5, 1], [150, 80, 150]);
+  const borderRadius = useMotionTemplate`0px 0px 0px 0px`;
 
-  const topElRef = useRef<HTMLDivElement>(null);
-  const bottomElRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const refs = imageRefs.current;
-    refs[1].scrollIntoView({ block: "center" });
-  }, []);
-
-  useEffect(() => {
-    imageScrollRef.current?.addEventListener("scroll", (event) => {
-      console.log("top", imageScrollRef.current?.scrollTop);
-    });
-
-    return () => {
-      window.removeEventListener("scroll", () => {
-        //
-      });
-    };
-  }, []);
-
-  const addItemToTop = () => {
-    setImages((prevImages) => {
-      const images = [...prevImages];
-      const lastTwo = images.splice(-1);
-
-      return [...lastTwo, ...images];
-    });
-
-    if (imageScrollRef.current) {
-      imageScrollRef.current.scrollTop += 540;
-    }
-  };
-
-  const addItemToBottom = () => {
-    setImages((prevImages) => {
-      const images = [...prevImages];
-      const firstTwo = images.splice(0, 1);
-
-      return [...images, ...firstTwo];
-    });
-  };
-
-  // infinite scroll observer
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (!entry.isIntersecting) return;
-          const scrollPosition = { topListener: "start", bottomListener: "end" }[entry.target.id];
-
-          if (scrollPosition === "start") {
-            addItemToTop();
-          }
-
-          if (scrollPosition === "end") {
-            console.log("add item to bottom");
-          }
-        });
-      },
-      { root: imageScrollRef.current, rootMargin: "80px", threshold: 0 }
-    );
-
-    const topEl = topElRef.current;
-    const bottomEl = bottomElRef.current;
-    if (topEl) {
-      observer.observe(topEl);
-    }
-
-    if (bottomEl) {
-      observer.observe(bottomEl);
-    }
-
-    return () => {
-      if (topEl) {
-        observer.unobserve(topEl);
-      }
-
-      if (bottomEl) {
-        observer.unobserve(bottomEl);
-      }
-    };
-  }, [images]);
+  // useEffect(() => {
+  //   if (id !== 2) return;
+  //   scrollYProgress.onChange((val) => {
+  //     console.log(val);
+  //   });
+  // }, [id, scrollYProgress]);
 
   return (
-    <div ref={imageScrollRef} className="fixed h-full overflow-auto top-0 right-28 z-10">
-      <button onClick={() => addItemToTop()} className="fixed top-0 left-0 bg-white p-6">
-        Add image
-      </button>
-
-      <div className="flex flex-col space-y-20">
-        <div id="topListener" ref={topElRef} />
-
-        {images.map((image, idx) => (
-          <div key={image.src} id={`${idx}`} ref={(el: any) => (imageRefs.current[idx] = el)}>
-            <Image width={360} height={620} src={image.src} className="object-cover rounded-[80px]" alt={image.alt} />
-          </div>
-        ))}
-
-        <div className="w-10 h-5 bg-red-50" id="bottomListener" ref={bottomElRef} />
-      </div>
-    </div>
+    <section className="h-screen flex items-center relative [scroll-snap-align:center] [perspective:500px]">
+      <motion.div
+        ref={ref}
+        className="w-[360px] h-[540px] relative max-h-[90vh] m-5 overflow-hidden"
+        style={{ borderRadius: radius }}
+        initial={{ y: "60vh" }}
+        animate={{ y: 0 }}
+        transition={{ duration: 1.8 }}
+      >
+        <motion.img src={`/images/${id}.jpg`} alt="A London skyscraper" style={{ borderRadius: radius }} />
+      </motion.div>
+    </section>
   );
 };
 
 const Home: NextPage = () => {
-  const [images, setImages] = useState(ALL_IMAGES);
-  const imageScrollRef = useRef<HTMLDivElement>(null);
-  const imageRefs = useRef<any[]>([]);
-
-  const intersectionObserverOptions = useMemo(
-    () => ({
-      root: imageScrollRef.current,
-      rootMargin: "0px",
-      threshold: 1.0,
-    }),
-    []
-  );
-
   return (
-    <div className="container mx-auto py-8">
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <>
       {/* initial opacity overlay */}
       <motion.div
         className="bg-[#6f543f] h-screen w-screen top-0 left-0 fixed pointer-events-none z-20"
@@ -171,87 +65,98 @@ const Home: NextPage = () => {
         animate={{ y: 0, opacity: 0.4, transition: { duration: 1.4 } }}
       ></motion.div>
 
-      {/* header */}
-      <motion.header
-        className="flex flex-row items-center text-[#f9e4d2]"
-        initial={{ x: 100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1.2, ease: "easeOut" }}
-      >
-        <h1 className="uppercase font-medium text-4xl">Defashe</h1>
-        <div className="flex flex-row ml-40">
-          <p className="text-xl">
-            <a className="hover:text-[#fcf1e9]" href="#">
-              Trends
-            </a>
-          </p>
-
-          <p className="ml-8 text-xl">
-            <a className="hover:text-[#fcf1e9]" href="#">
-              Collections
-            </a>
-          </p>
-
-          <p className="ml-8 text-xl">
-            <a className="hover:text-[#fcf1e9]" href="#">
-              Explore
-            </a>
-          </p>
-        </div>
-      </motion.header>
-
-      {/* body */}
-      <main className="min-h-[80vh] flex flex-col justify-center px-20 pr-60">
-        <div>
-          <motion.h2
-            className="text-9xl mb-6 tracking-wider text-[#ebe0cc] overflow-hidden"
-            initial={{ width: 0, opacity: 0 }}
-            animate={{ width: "auto", opacity: 1, transition: { duration: 2.5 } }}
+      {/* content */}
+      <div className="fixed top-0 left-0 w-screen h-screen">
+        <div className="container mx-auto py-8">
+          {/* header */}
+          <motion.header
+            className="flex flex-row items-center text-[#f9e4d2]"
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.2, ease: "easeOut" }}
           >
-            Incredible Collection
-          </motion.h2>
-          <div />
+            <h1 className="uppercase font-medium text-4xl">Defashe</h1>
+            <div className="flex flex-row ml-40">
+              <p className="text-xl">
+                <a className="hover:text-[#fcf1e9]" href="#">
+                  Trends
+                </a>
+              </p>
+
+              <p className="ml-8 text-xl">
+                <a className="hover:text-[#fcf1e9]" href="#">
+                  Collections
+                </a>
+              </p>
+
+              <p className="ml-8 text-xl">
+                <a className="hover:text-[#fcf1e9]" href="#">
+                  Explore
+                </a>
+              </p>
+            </div>
+          </motion.header>
+
+          {/* body */}
+          <main className="min-h-[80vh] flex flex-col justify-center px-20 pr-60">
+            <div>
+              <motion.h2
+                className="text-9xl mb-6 tracking-wider text-[#ebe0cc] overflow-hidden"
+                initial={{ width: 0, opacity: 0 }}
+                animate={{ width: "auto", opacity: 1, transition: { duration: 2.5 } }}
+              >
+                Incredible Collection
+              </motion.h2>
+              <div />
+            </div>
+
+            <motion.div
+              className="flex items-center w-fit ml-20 "
+              initial={{ x: -40, opacity: 0 }}
+              animate={{
+                x: 0,
+                opacity: 1,
+                transition: {
+                  duration: 1.2,
+                  ease: "easeOut",
+                },
+              }}
+              whileHover={{
+                x: 20,
+                transition: {
+                  duration: 0.8,
+                },
+              }}
+            >
+              <a className="text-3xl text-[#e3d0bf]" href="#">
+                Shop Now
+              </a>
+
+              <span className="w-20 h-[2px] block bg-[#e3d0bf] ml-4" />
+            </motion.div>
+          </main>
+
+          {/* copyright */}
+          <motion.div
+            className="w-fit"
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: -25, opacity: 1 }}
+            transition={{ duration: 1.2, ease: "easeOut" }}
+          >
+            <span className="text-xl mt-[30px] inline-block text-[#f9e4d2] rotate-[-90deg]">@2022</span>
+          </motion.div>
         </div>
-
-        <motion.div
-          className="flex items-center w-fit ml-20 "
-          initial={{ x: -40, opacity: 0 }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            transition: {
-              duration: 1.2,
-              ease: "easeOut",
-            },
-          }}
-          whileHover={{
-            x: 20,
-            transition: {
-              duration: 0.8,
-            },
-          }}
-        >
-          <a className="text-3xl text-[#e3d0bf]" href="#">
-            Shop Now
-          </a>
-
-          <span className="w-20 h-[2px] block bg-[#e3d0bf] ml-4" />
-        </motion.div>
-      </main>
+      </div>
 
       {/* images */}
-      <ImageList />
-
-      {/* copyright */}
-      <motion.div
-        className="w-fit"
-        initial={{ x: 100, opacity: 0 }}
-        animate={{ x: -25, opacity: 1 }}
-        transition={{ duration: 1.2, ease: "easeOut" }}
-      >
-        <span className="text-xl mt-[30px] inline-block text-[#f9e4d2] rotate-[-90deg]">@2022</span>
-      </motion.div>
-    </div>
+      <div className="z-10 absolute h-full top-0 right-28">
+        <motion.div className="flex flex-col" initial={{ y: "60vh" }} animate={{ y: 0 }} transition={{ duration: 1.8 }}>
+          {[1, 2, 3, 4].map((image) => (
+            <Image key={image} id={image} />
+          ))}
+        </motion.div>
+      </div>
+    </>
   );
 };
 
